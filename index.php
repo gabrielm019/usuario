6db4b959948f55052b7a909ee1af7bcd9c862fba
Gabriel Augusto <110576161+gabrielm019@users.noreply.github.com>
2023-03-02 22:26:30 -0300

    .

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
                     <div class="col-lg-8">
                         <h2 class="fw-bold mb-5">Sign up now</h2>
                         <form action="validar.php" method="post">
-                            <!-- 2 column grid layout with text inputs for the first and last names -->
+                            
                             <div class="row">
                                 <div class="col-md-6 mb-4">
                                     
@@ -38,7 +38,7 @@
                                
                             </div>
 
-                            <!-- Email input -->
+                            
                             <div class="form-outline mb-4">
                             <label class="form-label">Usuário</label>
                                 <input name="usuario"  class="form-control" />
